78aff897a16af2aca32a8b644709f1b76843b5e1
cam1271 <73512560+cam1271@users.noreply.github.com>
2022-07-28 23:38:54 -0500

    Add files via upload

--- path-planning/DLITE.py
+++ path-planning/DLITE.py
@@ -21,6 +21,8 @@ class DLITESEARCH:
 		self.img_height = len(segmentatedImage[0])
 		self.segmentatedImage = segmentatedImage
 		self.GroundTruthImage = GroundTruthImage
+		self.randarray = np.linspace(0, np.pi, 50)
+		self.pps = np.average(self.randarray)-self.randarray[0]
 		self.nodeTree = {}
 		self.path = []
 		self.changedNodeList = []
@@ -36,7 +38,6 @@ class DLITESEARCH:
 
 	def Initialize(self):
 		self.nodeTree[self.goal] = Node(cost=0, rhs=0)
-		print(self.CalculateKey(self.goal))
 		heapq.heappush(self.U, (self.CalculateKey(self.goal), self.goal))
 		self.open_Set_check.add(self.goal)
 
@@ -139,7 +140,6 @@ class DLITESEARCH:
 			elif self.nodeTree[head[1]].cost > self.nodeTree[head[1]].rhs:
 				self.nodeTree[head[1]].cost = self.nodeTree[head[1]].rhs
 				top = self.getPredecessorList(head[1])
-				print(top)
 				for lp in top:
 					self.nodeTree[lp].successors.add(head[1])
 					self.nodeTree[head[1]].predecessors.add(lp)
@@ -149,7 +149,6 @@ class DLITESEARCH:
 				self.nodeTree[head[1]].cost = np.inf
 				top = self.getPredecessorList(head[1])
 				top.append(head[1])
-				print(top, head[1])
 				for p in top:
 					self.UpdateVertex(p)
 
@@ -159,124 +158,98 @@ class DLITESEARCH:
 
 
 	def castRays(self, x,y):
-        final_dist = 100
-        angle_gen=0
-        angle_gen = math.atan2(y-self.current_location[1],x-self.current_location[0])
-        for xr in self.randarray:
-            x1 = int(x + final_dist * np.cos(xr+angle_gen-self.pps))
-            y1 = int(y + final_dist * np.sin(xr+angle_gen-self.pps))
-           # x1 = int(x+final_dist*np.cos(xr))
-            #y1 = int(y+final_dist*np.sin(xr))
-            self.bresenham((x,y), (x1,y1)) 
-             #   cv2.line(img_Copy,(x,y),(x1,y1),(0,255,0),1)
-            #else:
-             #   cv2.line(img_Copy, (x,y), lop[1], (0,0,255),1)
-
-            
-        
-        return 
+		final_dist = 100
+		angle_gen=0
+		angle_gen = math.atan2(y-self.start[1],x-self.start[0])
+		for xr in self.randarray:
+			x1 = int(x + final_dist * np.cos(xr+angle_gen-self.pps))
+			y1 = int(y + final_dist * np.sin(xr+angle_gen-self.pps))
+			self.bresenham((x,y), (x1,y1)) 
+		return 
 
 	def bresenham(self, start, end):
-        if start[0] < 0 or start[1] < 0 or start[1] >= len(self.initial_image) or start[0] >= len(self.initial_image) or np.array_equal(self.GroundTruthImage[start[1]][start[0]], [0,0,0]):
-            self.segmentatedImage[start[1]][start[0]] = self.GroundTruthImage[start[1]][start[0]]
-            return False
-        #, (start[0],start[1])
+		if start[0] < 0 or start[1] < 0 or start[1] >= len(self.segmentatedImage) or start[0] >= len(self.segmentatedImage) or np.array_equal(self.GroundTruthImage[start[1]][start[0]], [0,0,0]):
+			self.segmentatedImage[start[1]][start[0]] = self.GroundTruthImage[start[1]][start[0]]
+			self.changedNodeList.append(start)
+			return False
         
-        (x0, y0) = start
-        (x1, y1) = end
+		(x0, y0) = start
+		(x1, y1) = end
     
     
-        m = None
+		m = None
 
     
-        dx = abs(x1 - x0)
-        dy = abs(y1 - y0)
-        if dx == 0:
-            m = 1.0
-        else:
-            m = dy/dx
+		dx = abs(x1 - x0)
+		dy = abs(y1 - y0)
+		if dx == 0:
+			m = 1.0
+		else:
+			m = dy/dx
         
-        mm = False
+		mm = False
         # step 3 perform test to check if pk < 0
-        flag = True
+		flag = True
     
-        line_pixel = []
 
-      #  line_pixel.append((x0,y0))
     
-        step = 1
-        if x0>x1 or y0>y1:
-            step = -1
+		step = 1
+		if x0>x1 or y0>y1:
+			step = -1
         
-        if m < 1:
-            x0, x1 ,y0 ,y1 = y0, y1, x0, x1
-            dx = abs(x1 - x0)
-            dy = abs(y1 - y0)
-            mm = True
+		if m < 1:
+			x0, x1 ,y0 ,y1 = y0, y1, x0, x1
+			dx = abs(x1 - x0)
+			dy = abs(y1 - y0)
+			mm = True
         
-        p0 = 2*dx - dy
-        x = x0
-        y = y0
-        term = None
-        for i in range(abs(y1-y0)):
-            if flag:
-                x_previous = x0
-                p_previous = p0
-                p = p0
-                flag = False
-            else:
-                x_previous = x
-                p_previous = p
+		p0 = 2*dx - dy
+		x = x0
+		y = y0
+		term = None
+		for i in range(abs(y1-y0)):
+			if flag:
+				x_previous = x0
+				p_previous = p0
+				p = p0
+				flag = False
+			else:
+				x_previous = x
+				p_previous = p
         
     
-            if p >= 0:
-                x = x + step
+			if p >= 0:
+				x = x + step
 
-            p = p_previous + 2*dx -2*dy*(abs(x-x_previous))
-            y = y + step
+			p = p_previous + 2*dx -2*dy*(abs(x-x_previous))
+			y = y + step
         
-            if mm:
-             #   if np.array_equal(self.true_image[x][y], [0,0,0]) == False and np.array_equal(self.initial_image[x][y], [0,0,0]) and (y,x) in self.agent_tree:
-             #       self.agent_tree[(y,x)].f = self.eudis5((y,x), self.current_location)+self.agent_tree[(y,x)].cost
-                    
-                self.initial_image[x][y] = self.true_image[x][y]
+			if mm:
+				if x < 0 or y < 0 or y >= len(self.segmentatedImage) or x >= len(self.segmentatedImage):
+					return False
+				
+				if np.array_equal(self.segmentatedImage[x][y], self.GroundTruthImage[x][y]) == False:
+					self.changedNodeList.append((y,x))
+					self.segmentatedImage[x][y] = self.GroundTruthImage[x][y]
 
-                if x < 0 or y < 0 or y >= len(self.initial_image) or x >= len(self.initial_image) or  np.array_equal(self.true_image[x][y],[0,0,0]):
-                    if (y,x) in self.agent_tree:
-                        self.agent_tree[(y,x)].f = math.inf
-
-
-                    return False
+				if np.array_equal(self.GroundTruthImage[x][y],[0,0,0]):
+					return False
 
 
-            else:
-               # if np.array_equal(self.true_image[y][x], [0,0,0]) == False and np.array_equal(self.initial_image[y][x], [0,0,0]) and (x,y) in self.agent_tree:
-           #         self.agent_tree[(x,y)].f = self.eudis5((x,y), self.current_location)+self.agent_tree[(x,y)].cost
-                self.initial_image[y][x] = self.true_image[y][x]
+			else:
+				if x < 0 or y < 0 or y >= len(self.segmentatedImage) or x >= len(self.segmentatedImage):
+					return False
 
-                if x < 0 or x >= len(self.initial_image) or y < 0 or y >= len(self.initial_image) or np.array_equal(self.true_image[y][x],[0,0,0]):                  
-                    if (x,y) in self.agent_tree:
-                        self.agent_tree[(x,y)].f = math.inf
-                    
-                    
-                    return False
+
+				if np.array_equal(self.segmentatedImage[y][x], self.GroundTruthImage[y][x]) == False:
+					self.changedNodeList.append((x,y))
+					self.segmentatedImage[y][x] = self.GroundTruthImage[y][x]
+
+				if np.array_equal(self.GroundTruthImage[y][x],[0,0,0]):
+					return False
 
             
-        return True
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+		return True
 
 	def DLITERUN(self):
 		slast = self.start
@@ -287,12 +260,18 @@ class DLITESEARCH:
 				print('No path found.')
 				break
 			self.path.append(self.start)
-			succ_list = self.get_neighbors(self.start)
+			express = None
 			min_val = np.inf
+
+			succ_list = self.get_neighbors(self.start)
+
 			for p in succ_list:
 				if ((qg := self.nodeTree[p].cost + self.linkCostGrabber(p)) < min_val):
 					min_val = qg
-					self.start = p
+					express = p
+
+
+			self.castRays(express[0],express[1])
 
 			if len(self.changedNodeList) > 0:
 				self.h_cost = self.eudis5(self.start, self.goal)
@@ -302,4 +281,21 @@ class DLITESEARCH:
 				self.ComputeShortestPath()
 				self.changedNodeList = []
 
+
+
+			min_val = np.inf
+			express=None
+			for p in succ_list:
+				if ((qg := self.nodeTree[p].cost + self.linkCostGrabber(p)) < min_val):
+					min_val = qg
+					express = p
+
+			self.start = express
+			if self.start == None:
+				print('No path from this position found.')
+				break
+
+
+		if self.path[-1] == self.goal:
+			print('Path found.')
 		return self.path
